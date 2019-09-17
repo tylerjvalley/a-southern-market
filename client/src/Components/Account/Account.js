31478978300d95b78d0434c07ad61eacd8372f48
Tyler Valley <tvalley93@gmail.com>
2019-09-17 15:14:42 -0500

    Create login and register functionality

--- client/src/Components/Account/Account.js
+++ client/src/Components/Account/Account.js
@@ -6,13 +6,46 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import { Link } from 'react-router-dom';
-
+import axios from 'axios';
 import './Account.css';
 
 
 
 class Account extends Component {
+
     state = {
+        loginEmail: '',
+        loginPassword: '',
+    }
+
+
+    handleLoginInput = (type, e) => {
+
+        if (type === 'email') {
+            this.setState({ loginEmail: e.target.value })
+        } else if (type === 'password') {
+            this.setState({ loginPassword: e.target.value })
+        } else {
+            console.log('Something went wrong')
+        }
+    }
+
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        const user = {
+            email: this.state.loginEmail,
+            password: this.state.loginPassword
+        }
+
+        axios.post('http://localhost:5000/api/users/login', user)
+             .then(res => {
+                 console.log('Successfully logged in', res)
+             })
+             .catch(err => {
+                 console.log(err)
+             })
 
     }
 
@@ -26,18 +59,18 @@ class Account extends Component {
                             <Form>
                                 <Form.Group controlId="formBasicEmail">
                                     <Form.Label>Email address</Form.Label>
-                                    <Form.Control type="email" placeholder="Enter email" />
+                                    <Form.Control onChange={(e) => this.handleLoginInput('email', e)} type="email" placeholder="Enter email" />
                                     
                                 </Form.Group>
 
                                 <Form.Group controlId="formBasicPassword">
                                     <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" placeholder="Password" />
+                                    <Form.Control onChange={(e) => this.handleLoginInput('password', e)} type="password" placeholder="Password" />
                                 </Form.Group>
                                 <Form.Group controlId="formBasicChecbox">
                                     <Form.Check type="checkbox" label="Remember Me" />
                                 </Form.Group>
-                                <Button variant="secondary" type="submit">
+                                <Button onClick={(e) => this.handleSubmit(e)} variant="secondary" type="submit">
                                     Log In
                                 </Button>
                             </Form>
--- client/src/Components/Account/Register/Register.js
+++ client/src/Components/Account/Register/Register.js
@@ -3,6 +3,7 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './Register.css';
 
 
@@ -14,6 +15,53 @@ class Register extends Component {
         email: '',
         password: '',
         password2: '',
+    }
+
+
+    handleInput = (type, e) => {
+        
+        if (type === 'firstName') {
+            this.setState({ firstName: e.target.value } )
+        } else if (type === 'lastName') {
+            this.setState({ lastName: e.target.value } )
+        } else if (type === 'email') {
+            this.setState({ email: e.target.value } )
+        } else if (type === 'password') {
+            this.setState({ password: e.target.value } )
+        } else if (type === 'password2') {
+            this.setState({ password2: e.target.value } )
+        } else {
+            console.log('Something went wrong');
+        }
+
+
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        
+        const newUser = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            email: this.state.email,
+            password: this.state.password,
+            password2: this.state.password2
+        }
+
+        axios.post('http://localhost:5000/api/users/register', newUser)
+             .then(res => {
+                 console.log('Sucessfully Signed In', res)
+             })
+             .catch(err => {
+                 console.log(err);
+             })
+        
+
+        
+
+       
+
     }
 
 
@@ -29,29 +77,29 @@ class Register extends Component {
                     <Form>
                         <Form.Group controlId="formBasicFirst">
                             <Form.Label>First Name</Form.Label>
-                            <Form.Control type="email" placeholder="Enter first name" />
+                            <Form.Control onChange={(e) => this.handleInput('firstName', e)} type="email" placeholder="Enter first name" />
                         </Form.Group>
                         <Form.Group controlId="formBasicLast">
                             <Form.Label>Last Name</Form.Label>
-                            <Form.Control type="email" placeholder="Enter last name" />
+                            <Form.Control onChange={(e) => this.handleInput('lastName', e)} type="email" placeholder="Enter last name" />
                         </Form.Group>
-                        <Form.Group controlId="formBasicEmail">
+                        <Form.Group controlId="formEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control onChange={(e) => this.handleInput('email', e)} type="email" placeholder="Enter email" />
                         </Form.Group>
 
                         <Form.Group controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control onChange={(e) => this.handleInput('password', e)} type="password" placeholder="Password" />
                         </Form.Group>
                         <Form.Group controlId="formBasicPassword2">
                             <Form.Label>Confirm Password</Form.Label>
-                            <Form.Control type="password" placeholder="Re-enter Password" />
+                            <Form.Control onChange={(e) => this.handleInput('password2', e)} type="password" placeholder="Re-enter Password" />
                         </Form.Group>
                         <Form.Group controlId="formBasicChecbox">
                             <Form.Check type="checkbox" label="Remember Me" />
                         </Form.Group>
-                        <Link to="/account"><Button variant="success">Register</Button></Link>
+                        <Link to="/account"><Button onClick={(e) => this.handleSubmit(e)} variant="success">Register</Button></Link>
                     </Form>
                 </Container>
             </div>
