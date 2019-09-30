fa89430ab18cdb1f50b45459ef7d93b905f07715
Tyler Valley <tvalley93@gmail.com>
2019-09-30 16:37:59 -0500

    Remove webtoken and keys from user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
+
 
 
 // Load input validation
